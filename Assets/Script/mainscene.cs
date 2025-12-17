a9ee1dbc108fb671282843c0e5e0201fe938f864
C1auvios1eepyhead <ecm2209273@xmu.edu.my>
2025-12-17 13:20:54 +0800

    mainscene

---
+++ Assets/Script/mainscene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainscene : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game"); // 游戏场景名
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game"); // 编辑器测试用
+    }
+}

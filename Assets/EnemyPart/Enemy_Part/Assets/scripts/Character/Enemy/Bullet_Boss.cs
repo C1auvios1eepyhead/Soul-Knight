cf3a28b1c38929521d63880fe5f02590feef7ae8
CialloOwO <1329093542@qq.com>
2025-12-18 23:50:56 +0800

    Patrol State Bug Fixed

--- Assets/EnemyPart/Enemy_Part/Assets/scripts/Character/Enemy/Bullet_Boss.cs
+++ Assets/EnemyPart/Enemy_Part/Assets/scripts/Character/Enemy/Bullet_Boss.cs
@@ -5,7 +5,7 @@ public class Bullet : MonoBehaviour
     [Header("Bullet Settings")]
     public float speed = 10f;       // 飞行速度
     public float lifeTime = 5f;     // 存活时间
-    public float damage = 10f;      // 子弹伤害值，由Gun传入
+    public float damage = 10f;      // 子弹伤害值
 
     public LayerMask obstacleLayer;
 
@@ -26,8 +26,8 @@ public class Bullet : MonoBehaviour
         //只对有“Enemy”标签的敌人造成伤害
         if (collision.CompareTag("Player"))
         {
-            //敌人受击接口
-            collision.GetComponent<Character>().TakeDamage(damage);
+            //受击接口
+            collision.GetComponent<PlayerHealth>().TakeDamage(damage);
             Destroy(gameObject);
 
         }

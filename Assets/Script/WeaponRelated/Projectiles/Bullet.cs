cd4e9748ca513fdc1afa90ab580214e7d23e50ca
Christna28 <HuangJian1808721@gmail.com>
2025-12-19 22:03:04 +0800

    子弹可以击碎破坏物

--- Assets/Script/WeaponRelated/Projectiles/Bullet.cs
+++ Assets/Script/WeaponRelated/Projectiles/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Bullet : MonoBehaviour
 {
@@ -26,17 +27,31 @@ public class Bullet : MonoBehaviour
         {
             collision.GetComponent<Character>()?.TakeDamage(damage);
             Destroy(gameObject);
+            return;
         }
-        if (collision.gameObject.layer == LayerMask.NameToLayer("DestructibleTile"))
-        {
-            
-            Destroy(gameObject);
-        }
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
             Destroy(gameObject);
+            return;
         }
-        
-        
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("DestructibleTile"))
+        {
+            // 1) 拿到 Tilemap
+            Tilemap tilemap = collision.GetComponent<Tilemap>();
+            if (tilemap != null)
+            {
+                // 2) 用子弹当前位置决定打碎哪一格
+                Vector2 hitPoint = collision.ClosestPoint(transform.position);
+                Vector3Int cell = tilemap.WorldToCell(hitPoint);
+
+                if (tilemap.HasTile(cell))
+                    tilemap.SetTile(cell, null);
+            }
+
+            Destroy(gameObject);
+            return;
+        }
     }
 }

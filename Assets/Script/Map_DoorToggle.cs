84194307f06606c37960907693f546e0b7b8a525
Christna28 <HuangJian1808721@gmail.com>
2025-12-23 05:14:42 +0800

    三个音效和宝箱掉落优化

--- Assets/Script/Map_DoorToggle.cs
+++ Assets/Script/Map_DoorToggle.cs
@@ -9,6 +9,9 @@ public class Map_DoorToggle : MonoBehaviour
 
     private bool isClosed = false;
 
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip doorCloseSFX;
 
     private void Start()
     {
@@ -18,20 +21,37 @@ public class Map_DoorToggle : MonoBehaviour
 
     public void CloseDoor()
     {
+        if (isClosed) return; //  防止重复播放
+
         if (doorOpenLayer != null) doorOpenLayer.SetActive(false);
         if (doorCloseLayer != null) doorCloseLayer.SetActive(true);
+
         isClosed = true;
+
+        // 播放机关门音效
+        if (audioSource != null && doorCloseSFX != null)
+        {
+            audioSource.PlayOneShot(doorCloseSFX, 1.5f);
+        }
+
         ScanGraph1();
-    
-
-
     }
+
 
     public void OpenDoor()
     {
+        if (!isClosed) return; //  防止重复播放
+
         if (doorOpenLayer != null) doorOpenLayer.SetActive(true);
         if (doorCloseLayer != null) doorCloseLayer.SetActive(false);
+
         isClosed = false;
+
+        //  播放开门音效（复用石门音）
+        if (audioSource != null && doorCloseSFX != null)
+        {
+            audioSource.PlayOneShot(doorCloseSFX, 1.5f);
+        }
     }
 
     public bool IsClosed() => isClosed;
